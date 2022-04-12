3407f0025397839b8c00caee2a8068c7eb3846e6
polea003 <polea003@fiu.edu>
2022-04-12 07:56:17 -0400

    add tailwindcss to react project

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-red-500 bg-yellow-300'>Testing</h1>
+      <ol>
+        <li>one</li>
+        <li>two</li>
+      </ol>
     </div>
   );
 }
